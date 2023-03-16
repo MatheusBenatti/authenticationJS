62e0a283a1b55cbd64067164943084055622e334
BenaTTIX <theusbenatti@gmail.com>
2023-03-16 18:47:40 -0300

    db connection

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const app = express()
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
-    res.status(200).json({msg: "Bem vindo"})
+    res.status(200).json({msg: "Welcome"})
 })
 
-app.listen(3000)
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+
+
+app.post('/auth/register', async (req,res) => {
+    const {name, email, password, confirmpassword} = req.body
+
+    if(!name){
+        return res.status(422).json({msg: 'name is required'})
+    }
+})
+
+mongoose.connect(
+        `mongodb+srv://${dbUser}:${dbPass}@authjwt.h2j39mz.mongodb.net/?retryWrites=true&w=majority`,
+        )
+    .then(() => {
+    app.listen(3000)
+    console.log("connect")
+}).catch((err) => console.log(err))
+
